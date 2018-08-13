15b21245f5dce7a7921f2d41695cf35ee85df9ca
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-08-14 00:24:03 +0700

    init commit

---
+++ config/config.php
@@ -0,0 +1,23 @@
+
+<?php
+
+return [
+    'id' => 'micro-app',
+    // basePath (базовый путь) приложения будет каталог `micro-app`
+    'basePath' => __DIR__,
+    // это пространство имен где приложение будет искать все контроллеры
+    'controllerNamespace' => 'micro\controllers',
+    // установим псевдоним '@micro', чтобы включить автозагрузку классов из пространства имен 'micro'
+    'aliases' => [
+        '@micro' => __DIR__,
+    ],
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=example',
+            'username' => 'root',
+            'password' => '1234',
+            'charset' => 'utf8',
+        ],
+    ],
+];
